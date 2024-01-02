572ea337a42b2cb099071901587d521dba96220b
A107070014 <fesu308@gmail.com>
2024-01-02 15:24:34 +0800

    [feat]crud

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,22 +2,17 @@
   <q-page class="row q-pt-xl">
     <div class="full-width q-px-xl">
       <div class="q-mb-xl">
-        <q-input v-model="tempData.name" label="姓名" />
-        <q-input v-model="tempData.age" label="年齡" />
-        <q-btn color="primary" class="q-mt-md">新增</q-btn>
+        <q-input ref="nameRef" v-model="tempData.name" label="姓名 *"
+          :rules="[val => (val && val.length > 0) || '請輸入姓名']" />
+        <q-input ref="ageRef" type="number" v-model="tempData.age" label="年齡 *" :rules="[
+          val => (val !== null && val !== '') || '請輸入年齡',
+          val => (val >= 0) || '年齡必須大於零'
+        ]" />
+        <q-btn color="primary" class="q-mt-md" @click="validateForm">{{ isEdit ? '更新' : '新增' }}</q-btn>
       </div>
 
-      <q-table
-        flat
-        bordered
-        ref="tableRef"
-        :rows="blockData"
-        :columns="(tableConfig as QTableProps['columns'])"
-        row-key="id"
-        hide-pagination
-        separator="cell"
-        :rows-per-page-options="[0]"
-      >
+      <q-table flat bordered ref="tableRef" :rows="blockData" :columns="(tableConfig as QTableProps['columns'])"
+        row-key="id" hide-pagination separator="cell" :rows-per-page-options="[0]">
         <template v-slot:header="props">
           <q-tr :props="props">
             <q-th v-for="col in props.cols" :key="col.name" :props="props">
@@ -29,34 +24,14 @@
 
         <template v-slot:body="props">
           <q-tr :props="props">
-            <q-td
-              v-for="col in props.cols"
-              :key="col.name"
-              :props="props"
-              style="min-width: 120px"
-            >
+            <q-td v-for="col in props.cols" :key="col.name" :props="props" style="min-width: 120px">
               <div>{{ col.value }}</div>
             </q-td>
             <q-td class="text-right" auto-width v-if="tableButtons.length > 0">
-              <q-btn
-                @click="handleClickOption(btn, props.row)"
-                v-for="(btn, index) in tableButtons"
-                :key="index"
-                size="sm"
-                color="grey-6"
-                round
-                dense
-                :icon="btn.icon"
-                class="q-ml-md"
-                padding="5px 5px"
-              >
-                <q-tooltip
-                  transition-show="scale"
-                  transition-hide="scale"
-                  anchor="top middle"
-                  self="bottom middle"
-                  :offset="[10, 10]"
-                >
+              <q-btn @click="handleClickOption(btn, props.row)" v-for="(btn, index) in tableButtons" :key="index"
+                size="sm" color="grey-6" round dense :icon="btn.icon" class="q-ml-md" padding="5px 5px">
+                <q-tooltip transition-show="scale" transition-hide="scale" anchor="top middle" self="bottom middle"
+                  :offset="[10, 10]">
                   {{ btn.label }}
                 </q-tooltip>
               </q-btn>
@@ -64,10 +39,7 @@
           </q-tr>
         </template>
         <template v-slot:no-data="{ icon }">
-          <div
-            class="full-width row flex-center items-center text-primary q-gutter-sm"
-            style="font-size: 18px"
-          >
+          <div class="full-width row flex-center items-center text-primary q-gutter-sm" style="font-size: 18px">
             <q-icon size="2em" :name="icon" />
             <span> 無相關資料 </span>
           </div>
@@ -79,20 +51,33 @@
 
 <script setup lang="ts">
 import axios from 'axios';
-import { QTableProps } from 'quasar';
-import { ref } from 'vue';
+import { QTableProps, useQuasar } from 'quasar';
+import { ref, onMounted } from 'vue';
+const $q = useQuasar();
 interface btnType {
   label: string;
   icon: string;
   status: string;
 }
-const blockData = ref([
+interface dataType {
+  id: string,
+  creatorId: string,
+  name: string,
+  age: number | string,
+}
+interface tableConfigType {
+  label: string,
+  name: string,
+  field: string,
+  align: string,
+}
+const blockData = ref<dataType[]>([
   {
     name: 'test',
     age: 25,
   },
 ]);
-const tableConfig = ref([
+const tableConfig = ref<tableConfigType>([
   {
     label: '姓名',
     name: 'name',
@@ -106,7 +91,7 @@ const tableConfig = ref([
     align: 'left',
   },
 ]);
-const tableButtons = ref([
+const tableButtons = ref<btnType[]>([
   {
     label: '編輯',
     icon: 'edit',
@@ -119,12 +104,105 @@ const tableButtons = ref([
   },
 ]);
 
-const tempData = ref({
+const tableData = ref<dataType[]>([]);
+onMounted(() => {
+  getAllData();
+})
+async function getAllData() {
+  try {
+    const resData = await axios.get("/crudTest/a");
+    tableData.value = resData.data;
+  } catch (error) {
+    console.log("fetch error:", error);
+  }
+}
+
+const isEdit = ref<boolean>(false);
+const tempData = ref<dataType>({
   name: '',
   age: '',
 });
-function handleClickOption(btn, data) {
+const nameRef = ref(null);
+const ageRef = ref(null);
+
+function handleClickOption(btn: btnType, data: dataType) {
+  console.log("handleClickOption", btn, data);
+  switch (btn.status) {
+    case 'edit':
+      isEdit.value = true;
+      tempData.value.name = data.name;
+      tempData.value.age = data.age;
+      break;
+
+    case 'delete':
+      handleDeleteData(data.id);
+      break;
+  }
   // ...
+}
+function handleDeleteData(id: string) {
+  $q.dialog({
+    title: '提示',
+    message: '是否確定刪除該筆資料?',
+    cancel: true,
+    persistent: true
+  }).onOk(async () => {
+    // console.log('>>>> OK')
+    try {
+      const resData = await axios.delete(`/crudTest/${id}`);
+      getAllData();
+    } catch (error) {
+      console.log("fetch error:", error);
+    }
+  }).onOk(async () => {
+    // console.log('>>>> second OK catcher')
+    try {
+      const resData = await axios.delete(`/crudTest/${id}`);
+      getAllData();
+    } catch (error) {
+      console.log("fetch error:", error);
+    }
+  }).onCancel(() => {
+    // console.log('>>>> Cancel')
+  }).onDismiss(() => {
+    // console.log('I am triggered on both OK and Cancel')
+  })
+}
+
+function validateForm() {
+  nameRef.value.validate();
+  ageRef.value.validate();
+
+  if (nameRef.value.hasError || ageRef.value.hasError) return
+
+  if (isEdit.value) {
+    updateData();
+  } else {
+    createData();
+  }
+}
+async function createData() {
+  console.log("createData", tempData);
+  try {
+    const resData = await axios.post("/crudTest", tempData.value);
+    tempData.value.name = "";
+    tempData.value.age = "";
+    getAllData();
+  } catch (error) {
+    console.log("fetch error:", error);
+  }
+}
+async function updateData() {
+  console.log("updateData", tempData);
+  try {
+    const resData = await axios.patch("/crudTest", tempData.value);
+    tempData.value.name = "";
+    tempData.value.age = "";
+    isEdit.value = false;
+    getAllData();
+  } catch (error) {
+    console.log("fetch error:", error);
+  }
 }
 </script>
 
